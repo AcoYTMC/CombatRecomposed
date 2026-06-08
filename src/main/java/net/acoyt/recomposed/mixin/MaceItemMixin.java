69630b843ac327b6bf61a2ebd99c1c4921beb3fb
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-06-07 19:26:48 -0700

    Added mace damage limiter

--- src/main/java/net/acoyt/recomposed/mixin/MaceItemMixin.java
+++ src/main/java/net/acoyt/recomposed/mixin/MaceItemMixin.java
@@ -1,8 +1,10 @@
 package net.acoyt.recomposed.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.item.Item;
 import net.minecraft.item.MaceItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 /**
  * @author AcoYT
@@ -11,5 +13,10 @@ import org.spongepowered.asm.mixin.Mixin;
 public abstract class MaceItemMixin extends Item {
     public MaceItemMixin(Settings settings) {
         super(settings);
+    }
+
+    @ModifyReturnValue(method = "getBonusAttackDamage", at = @At("RETURN"))
+    private float recomposed$maceDamageDebuff(float original) {
+        return original * 0.75F;
     }
 }
