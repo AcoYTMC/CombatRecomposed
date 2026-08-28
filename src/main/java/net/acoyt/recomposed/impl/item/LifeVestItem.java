a08068a0adf261ef23e0c84a25fcfd52dc158ff9
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-08-27 20:40:52 -0700

    Reformatted mixins, added TJ's enchantment level suggestions, and added gold & netherite armor to the `strong` tag

--- src/main/java/net/acoyt/recomposed/impl/item/LifeVestItem.java
+++ src/main/java/net/acoyt/recomposed/impl/item/LifeVestItem.java
@@ -4,7 +4,6 @@ import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
-import net.acoyt.recomposed.impl.index.CRDataComponents;
 import net.acoyt.recomposed.impl.index.CRItems;
 import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvent;
@@ -14,6 +13,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+
 import java.util.Optional;
 
 /**
